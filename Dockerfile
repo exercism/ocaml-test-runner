506f3f99f03b1350a104a9e64e393d66adaa73ca
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-21 17:03:07 +0200

    Debug

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM ocaml/opam:debian-ocaml-4.14-afl
 
-RUN sudo apt-get update && \
-    sudo apt-get -y install m4 bmake cpio net-tools fswatch pkg-config jq && \
-    sudo apt-get purge --auto-remove && \
-    sudo apt-get clean && \
-    sudo rm -rf /var/lib/apt/lists/*
+# RUN sudo apt-get update && \
+#     sudo apt-get -y install m4 bmake cpio net-tools fswatch pkg-config jq && \
+#     sudo apt-get purge --auto-remove && \
+#     sudo apt-get clean && \
+#     sudo rm -rf /var/lib/apt/lists/*
 
-RUN opam update
-RUN opam install base calendar dune ounit react qcheck
+RUN sudo usermod -a -G root opam
 
-ENV PATH="/home/opam/.opam/4.14/bin:${PATH}"
+# RUN opam update
+# RUN opam install base calendar dune ounit react qcheck
+
+# ENV PATH="/home/opam/.opam/4.14/bin:${PATH}"
 
 WORKDIR /opt/test-runner
 COPY . .
--- bin/run.sh
+++ bin/run.sh
@@ -21,42 +21,49 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     exit 1
 fi
 
-slug="$1"
-input_dir="${2%/}"
-output_dir="${3%/}"
-exercise="${slug//-/_}"
-implementation_file="${input_dir}/${exercise}.pl"
-tests_file="${input_dir}/${exercise}_tests.plt"
-results_file="${output_dir}/results.json"
+whoami
+groups
 
-# Create the output directory if it doesn't exist
-mkdir -p "${output_dir}"
+ls -al $output_dir
 
-echo "${slug}: testing..."
+echo "test" > $output_dir/test.txt
 
-# Run the tests for the provided implementation file and redirect stdout and
-# stderr to capture it
-test_output=$(make -C ${input_dir} 2>&1)
+# slug="$1"
+# input_dir="${2%/}"
+# output_dir="${3%/}"
+# exercise="${slug//-/_}"
+# implementation_file="${input_dir}/${exercise}.pl"
+# tests_file="${input_dir}/${exercise}_tests.plt"
+# results_file="${output_dir}/results.json"
 
-# Write the results.json file based on the exit code of the command that was 
-# just executed that tested the implementation file
-if [ $? -eq 0 ]; then
-    jq -n '{version: 1, status: "pass"}' > ${results_file}
-else
-    # Sanitize the output
-    sanitized_test_output=$(echo "${test_output}" | sed -E \
-        -e "s/(make: (Entering|Leaving) directory '[^']+'|dune (clean|runtest))| *test alias runtest \(exit 1\)|\(cd _build[^\)]+\)//g" \
-        -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
-        -e 's/tests-[^\.]+\.log/tests.log/g' \
-        -e 's/in: [0-9]+\.[0-9]+ seconds\.//g' \
-        -e '/[F|\.]+$/d')
+# # Create the output directory if it doesn't exist
+# mkdir -p "${output_dir}"
 
-    # Manually add colors to the output to help scanning the output for errors
-    colorized_test_output=$(echo "$sanitized_test_output" \
-         | GREP_COLOR='01;31' grep --color=always -E \
-            -e '^(Error:.*|make:.*Error.*|FAILED:.*|Makefile:[0-9]+:.*failed)|$')
+# echo "${slug}: testing..."
 
-    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
-fi
+# # Run the tests for the provided implementation file and redirect stdout and
+# # stderr to capture it
+# test_output=$(make -C ${input_dir} 2>&1)
 
-echo "${slug}: done"
+# # Write the results.json file based on the exit code of the command that was 
+# # just executed that tested the implementation file
+# if [ $? -eq 0 ]; then
+#     jq -n '{version: 1, status: "pass"}' > ${results_file}
+# else
+#     # Sanitize the output
+#     sanitized_test_output=$(echo "${test_output}" | sed -E \
+#         -e "s/(make: (Entering|Leaving) directory '[^']+'|dune (clean|runtest))| *test alias runtest \(exit 1\)|\(cd _build[^\)]+\)//g" \
+#         -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
+#         -e 's/tests-[^\.]+\.log/tests.log/g' \
+#         -e 's/in: [0-9]+\.[0-9]+ seconds\.//g' \
+#         -e '/[F|\.]+$/d')
+
+#     # Manually add colors to the output to help scanning the output for errors
+#     colorized_test_output=$(echo "$sanitized_test_output" \
+#          | GREP_COLOR='01;31' grep --color=always -E \
+#             -e '^(Error:.*|make:.*Error.*|FAILED:.*|Makefile:[0-9]+:.*failed)|$')
+
+#     jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
+# fi
+
+# echo "${slug}: done"
