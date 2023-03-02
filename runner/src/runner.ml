0c3e2137ad8cec15992ad30e1c74f5c87ce0d49d
Andrei Formiga <archimedes_siracusa@hotmail.com>
2023-03-02 00:02:36 -0300

    Runner gets success/fail output from tests

--- runner/src/runner.ml
+++ runner/src/runner.ml
@@ -1,3 +1,4 @@
+open Ezxmlm
 
 let absolute_path path = 
   if Filename.is_relative path then 
@@ -19,20 +20,76 @@ let add_to_current_env var value =
   let new_var = Printf.sprintf "%s=%s" var value in 
   Array.append env [| new_var |] 
 
-let generate_json_results xml_path = 
-  if Sys.file_exists xml_path then 
-    ()
-  else
-    ()
+type case = { name: string; failmsg: string option } 
+type suite = { _tests: int; failures: int; errors: int; cases: case list }
 
-let root_object = 
+let check_case_failure case =
+  match members_with_attr "failure" case with 
+  | [] -> None
+  | [ attrs, _nodes ] -> Some (get_attr "message" attrs) 
+  | _ -> failwith "This should never happen"
+
+let read_case case_with_attrs = 
+  let attrs = fst case_with_attrs in
+  let name = get_attr "name" attrs in
+  match check_case_failure (snd case_with_attrs) with
+  | None -> { name; failmsg = None }
+  | Some m -> { name; failmsg = Some m }
+
+let read_xml_from_channel ch = 
+  let (_, xml) = from_channel ch in 
+  let suites = member "testsuites" xml |> members_with_attr "testsuite" in
+  let first_suite = List.hd suites in 
+  let suite_attrs = fst first_suite in
+  let n_tests = get_attr "tests" suite_attrs |> int_of_string in
+  let n_failures = get_attr "failures" suite_attrs |> int_of_string in 
+  let n_errors = get_attr "errors" suite_attrs |> int_of_string in 
+  { 
+    _tests = n_tests; 
+    failures = n_failures; 
+    errors = n_errors;
+    cases = members_with_attr "testcase" (snd first_suite) 
+            |> List.map read_case 
+  }
+   
+let read_xml fname = 
+  In_channel.with_open_text fname read_xml_from_channel
+
+
+let build_case_json case status msg = 
   `Assoc [
-    "version", `Int 2;
-    "status", `String "fail";
-    "message", `Null;
-    "tests", `List []
+    "name", `String case.name;
+    "status", `String status;
+    "message", msg;
+    "output", `Null;
+    "test_code", `Null
   ]
   
+let case_json case = 
+  match case.failmsg with 
+  | None -> build_case_json case "pass" `Null
+  | Some m -> build_case_json case "fail" (`String m)
+  
+let root_object status msg cases_json = 
+  `Assoc [
+    "version", `Int 2;
+    "status", `String status;
+    "message", msg;
+    "tests", `List cases_json
+  ]
+  
+let json_from_suite_results suite = 
+  match suite.failures, suite.errors with 
+  | 0, 0 -> root_object "pass" `Null (List.map case_json suite.cases)
+  | _, 0 -> root_object "fail" `Null (List.map case_json suite.cases)
+  | _, _ -> root_object "error" (`String "compiler error") []
+
+let generate_json_results xml_path = 
+  if Sys.file_exists xml_path then 
+    json_from_suite_results @@ read_xml xml_path
+  else
+    root_object "error" (`String "compiler error") []
+
 let xml_file_suffix = "junit-results.xml"
 
 let run_tests slug solution_dir output_dir = 
@@ -41,14 +98,15 @@ let run_tests slug solution_dir output_dir =
   let xml_path = Filename.concat abs_out_path xml_file in 
   let args = [| "make"; "-C"; solution_dir |] in 
   let env = add_to_current_env "OUNIT_OUTPUT_JUNIT_FILE" xml_path in 
-  let run_code = run_with_env "make" args env in 
-  false
+  let _run_code = run_with_env "make" args env in 
+  generate_json_results xml_path 
 
 let main slug solution_dir output_dir = 
-  if run_tests slug solution_dir output_dir then 
-    Printf.printf "***** OK!!!!"
-  else 
-    Printf.printf "***** ERROR!!!!!"
+  let abs_out_path = absolute_path output_dir in 
+  let out_file = Filename.concat abs_out_path "results.json" in
+  let json_object = run_tests slug solution_dir output_dir in 
+  Out_channel.with_open_text out_file @@ fun ch ->
+    Yojson.pretty_to_channel ch json_object 
 
 let () = 
   if Array.length Sys.argv < 4 then 
