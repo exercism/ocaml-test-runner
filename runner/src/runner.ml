2b1a316e08a0c23d24cee5f6b1bfb06a3edf152d
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-19 09:07:19 +0200

    Setup runner unit tests

--- runner/src/runner.ml
+++ runner/src/runner.ml
@@ -1,3 +1,4 @@
+open Case
 open Ezxmlm
 
 let version = 3
@@ -25,19 +26,6 @@ let add_to_current_env var value =
   let env = Unix.environment () in 
   let new_var = Printf.sprintf "%s=%s" var value in 
   Array.append env [| new_var |] 
-
-type case = { name: string; task_id: int option; failmsg: string option } 
-type suite = { _tests: int; failures: int; errors: int; cases: case list }
-
-let name_task_regexp = Str.regexp {|^\(.*\)\b @ task \([1-9]+\)$|}
-
-let case_of_name raw_name failmsg =
-  if Str.string_match name_task_regexp raw_name 0 then
-    let group i = Str.matched_group i raw_name in
-    let name = group 1
-    and task_id = group 2 |> int_of_string_opt
-    in { name; task_id; failmsg }
-  else {name = raw_name; task_id = None; failmsg }
 
 let check_case_failure case =
   match members_with_attr "failure" case with 
---
+++ runner/src/test.ml
@@ -0,0 +1,30 @@
+open OUnit2
+open Case
+
+let string_of_case case =
+  let pattern = Printf.sprintf "{ name = '%s'; task_id = %s; failmsg = '%s' }"
+  and task_id_repr = case.task_id |> Option.map string_of_int |> Option.value ~default:"None"
+  and failmsg_repr = case.failmsg |> Option.value ~default:"None"
+  in pattern case.name task_id_repr failmsg_repr
+
+let ae exp got _test_ctxt = assert_equal exp got ~printer:string_of_case
+
+let tests = [
+    "Nominal example" >::
+      ae { name = "Test"; task_id = Some 1; failmsg = Some "fail" } @@ case_of_name "Test @ task 1" (Some "fail") 
+    ; "Without task" >::
+      ae { name = "Test"; task_id = None; failmsg = None } @@ case_of_name "Test" None
+    ; "With task (multiple digits id)" >::
+      ae { name = "Test"; task_id = Some 123; failmsg = None } @@ case_of_name "Test @ task 123" None
+    ; "@ task without id" >::
+      ae { name = "Test @ task"; task_id = None; failmsg = None } @@ case_of_name "Test @ task" None
+    ; "@ task with id 0" >::
+      ae { name = "Test @ task 0"; task_id = None; failmsg = None } @@ case_of_name "Test @ task 0" None
+    ; "@ task with non integer id" >::
+      ae { name = "Test @ task foo"; task_id = None; failmsg = None } @@ case_of_name "Test @ task foo" None
+    ; "@ task with no test name" >::
+      ae { name = " @ task 1"; task_id = None; failmsg = None } @@ case_of_name " @ task 1" None
+  ]
+;;
+
+let () = run_test_tt_main ("runner tests" >::: tests)
